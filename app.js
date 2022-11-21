8b576cc1d6e19aaf18f44395ec954eb6c9f9ee48
thekiwidev <adedotungabriel20@gmail.com>
2022-11-21 22:11:16 +0100

    initial commit, kinda done with everything tho

---
+++ app.js
@@ -0,0 +1,10 @@
+// const vendorPlans = document.querySelectorAll(".vendor-plan");
+
+// vendorPlans.forEach((plan) => {
+//   plan.addEventListener("mouseenter", () => {
+//     plan.querySelector(".plan-features").classList.add("grow");
+//   });
+//   plan.addEventListener("mouseleave", () => {
+//     plan.querySelector(".plan-features").classList.remove("grow");
+//   });
+// });
